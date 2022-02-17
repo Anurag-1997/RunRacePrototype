71d041ce6f8381aa299397bd377c94c7521cb970
Anurag-1997 <eng.anurag@gmail.com>
2022-02-17 09:57:14 +0530

    added character controller
    move function initialized

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController characterController;
+    private Vector3 motion;
+    public float speed = 5;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        motion = Vector3.zero;
+
+        motion = transform.forward; //goes forward in player z direction
+
+        motion.Normalize();
+
+        motion = motion * speed * Time.deltaTime;
+
+        characterController.Move(motion);
+    }
+}
